f89f4db57ca71be5d90ecbd08508082d20c33267
fantagol <cesar.tradera1@gmail.com>
2026-08-06 13:36:47 -0500

    feat: enable immersive Android status bar behavior

--- android/app/src/main/java/app/fantagol/android/MainActivity.java
+++ android/app/src/main/java/app/fantagol/android/MainActivity.java
@@ -1,5 +1,53 @@
 package app.fantagol.android;
 
+import android.os.Bundle;
+import android.view.Window;
+
+import androidx.annotation.Nullable;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    private void hideStatusBar() {
+        Window window = getWindow();
+
+        WindowInsetsControllerCompat controller =
+                new WindowInsetsControllerCompat(
+                        window,
+                        window.getDecorView()
+                );
+
+        controller.setSystemBarsBehavior(
+                WindowInsetsControllerCompat
+                        .BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        );
+
+        controller.hide(
+                WindowInsetsCompat.Type.statusBars()
+        );
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        hideStatusBar();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        hideStatusBar();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        if (hasFocus) {
+            hideStatusBar();
+        }
+    }
+}
